979726f95a2694e0431c06f45285a931e890bc6d
msdev23 <serg-86@yandex.ru>
2020-03-27 18:07:13 +0900

    update navbar
    add function slideToggle

--- app/src/js/custom.js
+++ app/src/js/custom.js
@@ -7,8 +7,7 @@
 
     const burgerWrap = document.querySelector('.ms-burger-wrap');
     const burger = document.querySelector('.ms-burger');  
-    const nav = document.querySelector('.menu');  
-
+    const nav = document.querySelector('.menu');
 
     const handleClick = () => {    
         burger.classList.toggle('burger--active');
@@ -17,60 +16,61 @@
     }
     burgerWrap.addEventListener('click', handleClick);
 
-
     nav.addEventListener('click',function(e){
+        if (e.target.classList.contains('menu__toggler')) { 
 
-    if (e.target.classList.contains('menu__toggler')) { 
-  
-        const subMenuToggler = e.target;
-        const subMenu = subMenuToggler.nextElementSibling;
-        const showStatus = subMenuToggler.getAttribute('data-show');  
+            const subMenuToggler = e.target;
+            const subMenu = subMenuToggler.nextElementSibling;
+            const curShowStatus = subMenuToggler.getAttribute('data-show');
+
+            slideToggle(subMenu);
+    
+            let newShowStatus = ( curShowStatus == 'false' ) ? 'true' : 'false';
+    
+            subMenuToggler.setAttribute('data-show', newShowStatus);
+        }
+    })
+    
+
+    function getSubMenuHeight(el) {
+        let height = el.offsetHeight;
+        return height;
+    }
+
+    function slideToggle(el) {
+        let height = getSubMenuHeight(el);
         const transitionTime = 250; // css transition animation time
-        let height;
-        
-  
-        if ( showStatus == 'false' ) {        
+      
+        if ( height > 0 ) {
 
-            subMenu.style.display = 'block';
-            height = getSubMenuHeight();
-            subMenu.style.height = '0';
-
+            el.style.height = height + 'px';           
+            
             setTimeout(function() {                
-                subMenu.style.height = height + 'px';
+                el.style.height ='0';
 
                 setTimeout(function() {
-                    subMenu.classList.add('is-open');
-                    subMenu.style = '';                            
-                }, transitionTime);
+                    el.classList.remove('is-open');
+                    el.style = '';                            
+                }, 350);
             }, 0);
 
         } else {
 
-            height = getSubMenuHeight();
-            subMenu.style.height = height + 'px';           
-            
+            el.style.display = 'block';
+            height = getSubMenuHeight(el);
+            el.style.height = '0';
+
             setTimeout(function() {                
-                subMenu.style.height ='0';
+                el.style.height = height + 'px';
 
                 setTimeout(function() {
-                    subMenu.classList.remove('is-open');
-                    subMenu.style = '';                            
-                }, 350);
+                    el.classList.add('is-open');
+                    el.style = '';                            
+                }, transitionTime);
             }, 0);            
-
         }
-  
-        let newShowStatus = ( showStatus == 'false' ) ? 'true' : 'false';
-  
-        subMenuToggler.setAttribute('data-show', newShowStatus);
+    } // end slideToggle
 
-        function getSubMenuHeight() {
-            height = subMenu.offsetHeight;
-            return height;
-        }
-      
-    }
-  })
 
 })();
 
