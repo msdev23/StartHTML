137d50ab276bbfcdd203b283cdf3f3f109d2667c
msdev23 <serg-86@yandex.ru>
2020-03-27 23:12:14 +0900

    navbar add accordion effect for moble version

--- app/src/js/custom.js
+++ app/src/js/custom.js
@@ -25,7 +25,47 @@
             const subMenu = subMenuToggler.nextElementSibling;
             const curShowStatus = subMenuToggler.getAttribute('data-show');
 
-            slideToggle(subMenu);
+
+            // Преждем чем открыть, скроем другие (акордеон)
+            /*
+                При клике на кнопку надо закрыть остальные открытые (на своём уровне)
+                Надо найти все соседние элементы - поднимемся к родителю кнопки и 
+                дальше к общему родителю соседей и текущего
+                Посчитаем их и с помощью for переберём и найдём нужные (is-open)
+                когда нашли - зайдём внутрь и закроем sub-menu
+                Только потом отработаем открытие нового суб-меню
+             */
+
+            
+            const parent = subMenuToggler.closest('.menu__item--has-children').parentElement; // родитель открываемого пункта
+            const siblingsCount = parent.children.length; // считаем кол-во элементов на это уровне            
+            const sibling = parent.children; // получаем все дочерние узлы
+
+            // найдём соседа в котором уже открыто sub-menu
+            if ( siblingsCount > 0 ) {
+                for (let i = 0; i < siblingsCount; i++ ){                    
+                    if ( sibling[i].classList.contains('is-open')  ) {                        
+                        
+                        // найдём то самое sub-menu
+                        const childList = sibling[i].children; // получаем всех детей
+                        const childListCount = sibling[i].children.length; // считаем
+                        if ( childListCount > 0 ) { // перебираем 
+                            for (let n = 0; n < childListCount; n++){
+
+                                if ( childList[n].classList.contains('is-open') ) {
+                  
+                                    slideToggle(childList[n]); // закрываем
+                                }
+                            }
+                        }                    
+
+
+                    }
+                }
+            }
+
+            // откроем
+            slideToggle(subMenu);  
     
             let newShowStatus = ( curShowStatus == 'false' ) ? 'true' : 'false';
     
@@ -53,6 +93,7 @@
 
                 setTimeout(function() {
                     el.classList.remove('is-open');
+                    el.parentElement.classList.remove('is-open');
                     el.style = '';                            
                 }, 350);
             }, 0);
@@ -68,12 +109,13 @@
 
                 setTimeout(function() {
                     el.classList.add('is-open');
+                    el.parentElement.classList.add('is-open');
                     el.style = '';                            
                 }, transitionTime);
             }, 0);            
         }
     } // end slideToggle
-
+ 
 })();
 
 
